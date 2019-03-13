eb984a5d7cc28bc00203982681a7c6d19499c519
khwilo <khwilowatai@gmail.com>
2019-03-13 19:41:46 +0300

    Include view for adding todo items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -40,11 +41,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Todos
-          todos          = {this.state.todos}
-          toggleComplete = {this.toggleComplete}
-          deleteTodo     = {this.deleteTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos
+            todos          = {this.state.todos}
+            toggleComplete = {this.toggleComplete}
+            deleteTodo     = {this.deleteTodo}
+          />
+          </div>
       </div>
     );
   }
