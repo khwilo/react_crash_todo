4936b64128c87b5aa9bb646f1f00501616eadc4a
khwilo <khwilowatai@gmail.com>
2019-03-13 13:07:18 +0300

    Use a function for styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+    getStyle = () => {
+        if(this.props.todo.completed) {
+            return {
+                textDecoration: 'line-through'
+            }
+        } else {
+            return {
+                textDecoration: 'none'
+            }
+        }
+    }
     render() {
         return (
-            <div style={itemStyle}>
+            <div style={this.getStyle()}>
                 <p>{this.props.todo.title}</p>
             </div>
         );
@@ -13,10 +24,6 @@ class TodoItem extends Component {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
-}
-
-const itemStyle = {
-    backgroundColor: "#f4f4f4"
 }
 
 export default TodoItem;
