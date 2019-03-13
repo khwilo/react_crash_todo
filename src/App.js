272527a94a901a719bb4bd9c4ac496f73a6e826f
khwilo <khwilowatai@gmail.com>
2019-03-13 12:16:38 +0300

    Add Todos state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id       : 1,
+        title    : "Apply for job",
+        completed: false
+      },
+      {
+        id       : 2,
+        title    : "RSVP to meetup",
+        completed: false
+      },
+      {
+        id       : 3,
+        title    : "Watch The Wire",
+        completed: false
+      }
+    ]
+  }
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
         <Todos />
